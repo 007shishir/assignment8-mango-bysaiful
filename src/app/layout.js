ea6c7b210dea39f89af3511db807f88c17d01bd5
007shishir <islam7.saiful@gmail.com>
2026-05-04 21:08:10 +0600

    I do not know what I am doing

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,6 @@
+import dns from "dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
